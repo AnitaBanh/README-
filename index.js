77588961c1b0f1daa820d7420a1793eec0d9d66a
AB <anitabanhtx@gmail.com>
2023-01-23 16:27:04 -0600

    add prompts

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
 const questions = [];
-
+inquirer.prompt([
+  {
+    type: "input",
+    message: "What is the project title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the project description?",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What are the installation instructions?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "What is the usage information?",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "What are the contribution guidelines?",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "What are the test instructions?",
+    name: "testInstructions",
+  },
+  {
+    type: "input",
+    message: "What license is applicable to his project?",
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "username",
+  },
+]);
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
